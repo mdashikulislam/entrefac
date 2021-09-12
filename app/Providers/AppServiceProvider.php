a42e9bb099d8b86b9f8ebfd719e2426d842c39a9
mdashikulislam <ashik.nwu@gmail.com>
2021-09-13 00:23:41 +0600

    update config

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        \Schema::defaultStringLength(191);
+        if (env('APP_ENV') !== 'local'){
+            URL::forceScheme('https');
+        }
     }
 }
